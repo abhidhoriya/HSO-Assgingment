89a8b04482b2bc2a43db34b9ff71090ec2fd1ad5
ABHISHEK M DHORIYA <abdhoriy@microsoft.com>
2024-01-19 16:46:24 +0530

    removed the debugger and console references.

--- HSO.CrmPackage/WebResources/Scripts (Jscript)/Account/account.js
+++ HSO.CrmPackage/WebResources/Scripts (Jscript)/Account/account.js
@@ -3,7 +3,7 @@ var HSO = HSO || { namespace: true };
 HSO.Account = HSO.Account || { namespace: true };
 HSO.Account.Fields = {
     DisplayName: "name",
-    BankNumber: "hso_bankaccount"
+    BankNumber:"hso_bankaccount"
 };
 HSO.Account.Constants = {
     IBANApiKey: "820d953419e4c3bf9f84c753310ca480b2815fed",
@@ -21,20 +21,13 @@ HSO.Account.onSave = function (executionContext) {
         var isbankNumberFieldChanged = formContext.getAttribute(HSO.Account.Fields.BankNumber) === null ? false : formContext.getAttribute(HSO.Account.Fields.BankNumber).getIsDirty();
         if (isbankNumberFieldChanged) {
             var bankNumber = formContext.getAttribute(HSO.Account.Fields.BankNumber) === null ? null : formContext.getAttribute(HSO.Account.Fields.BankNumber).getValue();
-            debugger;
             var ibanApi = IBANApi();
             ibanApi.validateIBANBasic(bankNumber, HSO.Account.Constants.IBANApiKey).then(function (jsonoutputdata) {
-                debugger;
                 if (jsonoutputdata.result != 200) {
-                    console.log(jsonoutputdata);
                     Xrm.Navigation.openAlertDialog({ text: "" + jsonoutputdata.message });
                     formContext.getAttribute(HSO.Account.Fields.BankNumber).setValue("");
                     executionContext.getEventArgs().preventDefault();
                 }
-                else {
-                    console.log(jsonoutputdata);
-                }
-
             });
         }
     }
